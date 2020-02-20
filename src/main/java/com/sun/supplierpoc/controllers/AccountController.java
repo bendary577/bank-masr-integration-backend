e0239622cf13d1c0ca247bca47f147acccef3d5d
M.Amir090 <mamir@entrepreware.com>
2020-02-20 17:47:12 +0200

    - adding oauth2 with resources id @ENTREPREWARE and auth endpoints "oauth/token" with grant type password and refresh_token

--- src/main/java/com/sun/supplierpoc/controllers/AccountController.java
+++ src/main/java/com/sun/supplierpoc/controllers/AccountController.java
@@ -1,26 +1,94 @@
 package com.sun.supplierpoc.controllers;
 
+import com.google.common.collect.Sets;
+import com.sun.supplierpoc.conf.CustomClientDetailsService;
+import com.sun.supplierpoc.models.AccessTokenResponse;
 import com.sun.supplierpoc.models.Account;
+import com.sun.supplierpoc.models.RefreshTokenResult;
+import com.sun.supplierpoc.models.auth.MongoClientDetails;
+import com.sun.supplierpoc.models.auth.MongoUser;
 import com.sun.supplierpoc.repositories.AccountRepo;
+import com.sun.supplierpoc.services.security.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.http.*;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Optional;
-
+@EnableResourceServer
 @RestController
-
 public class AccountController {
     @Autowired
     private AccountRepo accountRepo;
+    @Autowired
+    private RestTemplate restTemplate;
 
+    @Value("${oauth.auth.url}")
+    private String authUrl;
 
+    @Value("${oauth.token.url}")
+    private String accessTokenUrl;
+
+    @Value("${oauth.token.refresh.url}")
+    private String refreshTokenUrl;
+
+    @Value("${oauth.id.secret}")
+    private String encodedIdSecret;
     ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    @Autowired
+    CustomClientDetailsService customClientDetailsService;
+    @Autowired
+    UserDetailsService userDetailsService;
+    @Autowired
+    MongoTemplate mongoTemplate;
 
     @RequestMapping("/getAccount")
     @CrossOrigin(origins = "*")
     @ResponseBody
-    public Optional<Account> getAccount(){
+    public Optional<Account> getAccount() {
+
         // get accountID from user 5e4bd1a7b334d338f81d9b9a
         return accountRepo.findById("5e4bd1a7b334d338f81d9b9a");
     }
+
+    @RequestMapping(value = "/clientDetails")
+    @ResponseBody
+    public ResponseEntity<RefreshTokenResult> clientDetails(Authentication authentication) {
+        MongoClientDetails clientDetails = new MongoClientDetails();
+        clientDetails.setClientId("web-client");
+        clientDetails.setClientSecret("web-client-secret");
+        clientDetails.setSecretRequired(true);
+        clientDetails.setResourceIds(Sets.newHashSet("project-man"));
+        clientDetails.setScope(Sets.newHashSet("call-services"));
+        clientDetails.setAuthorizedGrantTypes(Sets.newHashSet("authorization_code", "refresh_token"));
+        clientDetails.setRegisteredRedirectUri(Sets.newHashSet("http://localhost:8080"));
+        clientDetails.setAuthorities(AuthorityUtils.createAuthorityList("ROLE_USER"));
+        clientDetails.setAccessTokenValiditySeconds(60);
+        clientDetails.setRefreshTokenValiditySeconds(14400);
+        clientDetails.setAutoApprove(false);
+        customClientDetailsService.addClientDetails(clientDetails);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value = "/user")
+    @ResponseBody
+    public ResponseEntity<RefreshTokenResult> adduser() {
+        MongoUser mongoUser = new MongoUser();
+        mongoUser.setUsername("user");
+        mongoUser.setPassword(new BCryptPasswordEncoder(12).encode("user"));
+        mongoUser.setRoles(Sets.newHashSet("ROLE_USER"));
+        mongoTemplate.save(mongoUser);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
 }
---
+++ src/main/java/com/sun/supplierpoc/services/security/CustomApprovalStore.java
@@ -0,0 +1,129 @@
+package com.sun.supplierpoc.services.security;
+
+
+import com.mongodb.WriteResult;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
+import com.sun.supplierpoc.models.auth.MongoApproval;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
+import org.springframework.security.oauth2.provider.approval.Approval;
+import org.springframework.security.oauth2.provider.approval.ApprovalStore;
+
+import java.util.*;
+
+/**
+ * Created by jeebb on 11/10/14.
+ */
+public class CustomApprovalStore implements ApprovalStore {
+
+    @Autowired
+    private MongoTemplate mongoTemplate;
+
+    private boolean handleRevocationsAsExpiry = false;
+
+
+    public boolean addApprovals(Collection<Approval> approvals) {
+        boolean isSuccess = true;
+        Iterator<Approval> iterator = approvals.iterator();
+        while (iterator.hasNext()) {
+            Approval approval = iterator.next();
+            if (!updateApproval(approval) && !addApproval(approval)) {
+                isSuccess = false;
+            }
+        }
+        return isSuccess;
+    }
+
+    private boolean updateApproval(Approval approval) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where(MongoApproval.USER_ID).is(approval.getUserId()));
+        query.addCriteria(Criteria.where(MongoApproval.CLIENT_ID).is(approval.getClientId()));
+        query.addCriteria(Criteria.where(MongoApproval.SCOPE).is(approval.getScope()));
+
+        Update update = new Update();
+        update.set(MongoApproval.EXPIRE_AT, approval.getExpiresAt().getTime());
+        update.set(MongoApproval.STATUS, approval.getStatus() != null ? approval.getStatus() : Approval.ApprovalStatus.APPROVED);
+        update.set(MongoApproval.LAST_MODIFIED_AT, approval.getLastUpdatedAt().getTime());
+
+        UpdateResult writeResult = mongoTemplate.updateFirst(query, update, MongoApproval.class);
+        return (writeResult.getMatchedCount() > 0);
+    }
+
+    private boolean addApproval(Approval approval) {
+        MongoApproval mongoApproval = new MongoApproval();
+        mongoApproval.setUserId(approval.getUserId());
+        mongoApproval.setClientId(approval.getClientId());
+        mongoApproval.setScope(approval.getScope());
+        mongoApproval.setExpireAt(approval.getExpiresAt().getTime());
+        mongoApproval.setLastModifiedAt(approval.getLastUpdatedAt().getTime());
+        mongoApproval.setStatus(approval.getStatus() != null ? approval.getStatus() : Approval.ApprovalStatus.APPROVED);
+        try {
+            mongoTemplate.save(mongoApproval);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public boolean revokeApprovals(Collection<Approval> approvals) {
+        boolean isSuccess = true;
+        Iterator<Approval> iterator = approvals.iterator();
+
+        while (iterator.hasNext()) {
+            Approval approval = iterator.next();
+            if (handleRevocationsAsExpiry) {
+                Query query = new Query();
+                query.addCriteria(Criteria.where(MongoApproval.USER_ID).is(approval.getUserId()));
+                query.addCriteria(Criteria.where(MongoApproval.CLIENT_ID).is(approval.getClientId()));
+                query.addCriteria(Criteria.where(MongoApproval.SCOPE).is(approval.getScope()));
+
+                Update update = new Update();
+                update.set(MongoApproval.EXPIRE_AT, System.currentTimeMillis());
+
+                UpdateResult writeResult = mongoTemplate.updateFirst(query, update, MongoApproval.class);
+                isSuccess = (writeResult.getMatchedCount() == 1);
+            } else {
+                Query query = new Query();
+                query.addCriteria(Criteria.where(MongoApproval.USER_ID).is(approval.getUserId()));
+                query.addCriteria(Criteria.where(MongoApproval.CLIENT_ID).is(approval.getClientId()));
+                query.addCriteria(Criteria.where(MongoApproval.SCOPE).is(approval.getScope()));
+
+                DeleteResult writeResult = mongoTemplate.remove(query, MongoApproval.class);
+                isSuccess = (writeResult.getDeletedCount() == 1);
+            }
+        }
+        return isSuccess;
+    }
+
+
+    public Collection<Approval> getApprovals(String username, String clientId) {
+        Collection<Approval> approvals = new ArrayList<Approval>();
+
+        Query query = new Query();
+        query.addCriteria(Criteria.where(MongoApproval.CLIENT_ID).is(clientId));
+        query.addCriteria(Criteria.where(MongoApproval.USER_ID).is(username));
+
+        List<MongoApproval> mongoApprovals = mongoTemplate.find(query, MongoApproval.class);
+
+        for (MongoApproval mongoApproval : mongoApprovals) {
+            approvals.add(new Approval(mongoApproval.getUserId(), mongoApproval.getClientId(),
+                    mongoApproval.getScope(), new Date(mongoApproval.getExpireAt()), mongoApproval.getStatus(),
+                    new Date(mongoApproval.getLastModifiedAt())));
+        }
+
+        return approvals;
+    }
+
+    public boolean isHandleRevocationsAsExpiry() {
+        return handleRevocationsAsExpiry;
+    }
+
+    public void setHandleRevocationsAsExpiry(boolean handleRevocationsAsExpiry) {
+        this.handleRevocationsAsExpiry = handleRevocationsAsExpiry;
+    }
+}

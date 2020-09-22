6de76ef476c08c831fa0b19cee982a013a523825
Laura <lyoussef@entrepreware.com>
2020-09-22 15:07:45 +0200

    add new sync job types in adding account method

--- src/main/java/com/sun/supplierpoc/controllers/AccountController.java
+++ src/main/java/com/sun/supplierpoc/controllers/AccountController.java
@@ -123,51 +123,73 @@ public class AccountController {
         userRepo.save(user);
 
         // add default sync jobs to account
-        addAccountSyncType(account);
+        boolean addingStatus = addAccountSyncType(account);
 
-        response.put("message", "Account added successfully.");
-        response.put("success", true);
+        if(addingStatus){
+            response.put("message", "Account added successfully.");
+            response.put("success", true);
+        }else{
+            response.put("message", "Failed to add new account.");
+            response.put("success", false);
+        }
         return response;
     }
 
-    public boolean addAccountSyncType(Account account){
-        // suppliers
-        String syncDescription = "Used to sync suppliers from sun to my inventory daily.";
-        SyncJobType supplierSyncType = new SyncJobType(Constants.SUPPLIERS, syncDescription, "/suppliers",
-                new Date(), new Configuration(), account.getId());
-        syncJobTypeRepo.save(supplierSyncType);
+    private boolean addAccountSyncType(Account account){
+        try {
+            // suppliers
+            String syncDescription = "Used to sync suppliers from sun to my inventory daily.";
+            SyncJobType supplierSyncType = new SyncJobType(1,Constants.SUPPLIERS, syncDescription, "/suppliers",
+                    new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(supplierSyncType);
 
-        // Invoices
-        syncDescription = "Used to sync approved invoices from my inventory to sun daily.";
-        SyncJobType invoiceSyncType = new SyncJobType(Constants.APPROVED_INVOICES, syncDescription, "/approvedInvoices",
-                new Date(), new Configuration(), account.getId());
-        syncJobTypeRepo.save(invoiceSyncType);
+            // Invoices
+            syncDescription = "Used to sync approved invoices from my inventory to sun daily.";
+            SyncJobType invoiceSyncType = new SyncJobType(2, Constants.APPROVED_INVOICES, syncDescription, "/approvedInvoicesSun",
+                    new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(invoiceSyncType);
 
-        // Credit Notes
-        syncDescription = "Used to sync credit notes from my inventory to sun daily.";
-        SyncJobType creditNotesSyncType = new SyncJobType(Constants.CREDIT_NOTES, syncDescription, "/creditNotes",
-                new Date(), new Configuration(), account.getId());
-        syncJobTypeRepo.save(creditNotesSyncType);
+            // Credit Notes
+            syncDescription = "Used to sync credit notes from my inventory to sun daily.";
+            SyncJobType creditNotesSyncType = new SyncJobType(3, Constants.CREDIT_NOTES, syncDescription, "/creditNotesSun",
+                    new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(creditNotesSyncType);
 
-        // Transfers
-        syncDescription = "Used to sync transfers from my inventory to sun monthly.";
-        SyncJobType transferSyncType = new SyncJobType(Constants.TRANSFERS, syncDescription, "/bookedTransferSun",
-                new Date(), new Configuration(), account.getId());
-        syncJobTypeRepo.save(transferSyncType);
+            // Wastage
+            syncDescription = "Used to sync wastage from oracle hospitality reports to sun monthly.";
+            SyncJobType wastageSyncType = new SyncJobType(4, Constants.WASTAGE, syncDescription, "/wastageSun",
+                    new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(wastageSyncType);
 
-        // Consumption
-        syncDescription = "Used to sync consumption from oracle hospitality reports to sun monthly.";
-        SyncJobType consumptionSyncType = new SyncJobType(Constants.CONSUMPTION, syncDescription, "/journalsSun",
-                new Date(), new Configuration(), account.getId());
-        syncJobTypeRepo.save(consumptionSyncType);
+            // Transfers
+            syncDescription = "Used to sync transfers from my inventory to sun monthly.";
+            SyncJobType transferSyncType = new SyncJobType(5, Constants.TRANSFERS, syncDescription, "/bookedTransferSun",
+                    new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(transferSyncType);
 
-        // Wastage
-        syncDescription = "Used to sync wastage from oracle hospitality reports to sun monthly.";
-        SyncJobType wastageSyncType = new SyncJobType(Constants.WASTAGE, syncDescription, "/wastageSun",
-                new Date(), new Configuration(), account.getId());
-        syncJobTypeRepo.save(wastageSyncType);
+            // Booked Production
+            syncDescription = "Used to sync booked production from my inventory to sun monthly.";
+            SyncJobType bookedProductionSyncType = new SyncJobType(6, Constants.BOOKED_PRODUCTION, syncDescription,
+                    "/bookedProductionSun", new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(bookedProductionSyncType);
 
-        return true;
+            // Sales
+            syncDescription = "Used to sync sales from oracle hospitality reports to sun monthly.";
+            SyncJobType salesSyncType = new SyncJobType(7, Constants.SALES, syncDescription, "/posSalesSun",
+                    new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(salesSyncType);
+
+            // Consumption
+            syncDescription = "Used to sync consumption from oracle hospitality reports to sun monthly.";
+            SyncJobType consumptionSyncType = new SyncJobType(8, Constants.CONSUMPTION, syncDescription, "/consumptionSun",
+                    new Date(), new Configuration(), account.getId());
+            syncJobTypeRepo.save(consumptionSyncType);
+
+            return true;
+        } catch (Exception e) {
+            return false;
+        }
+
     }
 
     public ArrayList<Account> getAccounts() {

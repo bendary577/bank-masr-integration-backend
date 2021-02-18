c2ddd8beb1a08fbca01a0b784bcb7a41fbd33f69
Laura <lyoussef@entrepreware.com>
2021-02-18 13:09:17 +0200

    generate QR code

---
+++ src/main/java/com/sun/supplierpoc/controllers/application/AppUserController.java
@@ -0,0 +1,37 @@
+package com.sun.supplierpoc.controllers.application;
+import com.sun.supplierpoc.services.QRCodeGenerator;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping(value = {"/Simphony"})
+
+public class AppUserController {
+    @Autowired
+    QRCodeGenerator qrCodeGenerator;
+
+    private static final String QR_CODE_IMAGE_PATH = "./src/main/resources/QRCode.png";
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    @GetMapping(path = "/generateAndDownloadQRCode")
+    public void generateAndDownloadQRCode(
+            @RequestParam(name = "codeText") String codeText,
+            @RequestParam(name = "width") Integer width,
+            @RequestParam(name = "height") Integer height)
+            throws Exception {
+        qrCodeGenerator.generateQRCodeImage(codeText, width, height, QR_CODE_IMAGE_PATH);
+    }
+
+    @GetMapping(value = "/generateQRCode")
+    public ResponseEntity<byte[]> generateQRCode(
+            @RequestParam(name = "codeText") String codeText,
+            @RequestParam(name = "width") Integer width,
+            @RequestParam(name = "height") Integer height)
+            throws Exception {
+        return ResponseEntity.status(HttpStatus.OK).body(qrCodeGenerator.getQRCodeImage(codeText, width, height));
+    }
+}

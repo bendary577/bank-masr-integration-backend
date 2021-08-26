5e82eec2202a1810f3b94c1ef4f4a5ea7710353a
mario130 <mario.yonan.work@gmail.com>
2021-08-26 14:01:48 +0200

    nesting order items

--- src/main/java/com/sun/supplierpoc/models/Order.java
+++ src/main/java/com/sun/supplierpoc/models/Order.java
@@ -11,98 +11,26 @@ import java.util.List;
 public class Order implements Serializable {
     @Id
     private ObjectId _id = ObjectId.get();
-    private String name;
-    private int quantity;
-    private String size;
-    private List<CondimentResponse> toppings;
-    private double price;
-    private int spicyLevel;
-    private String imageUrl;
-    private Date creationDate = new Date();
+    private List<SingleOrder> orderMeals;
     private boolean deleted = false;
 
     public Order() {
     }
 
-    public Order(String name, int quantity, String size, List<CondimentResponse> toppings,
-                 double price, int spicyLevel, String imageUrl) {
-        this.name = name;
-        this.quantity = quantity;
-        this.size = size;
-        this.toppings = toppings;
-        this.price = price;
-        this.spicyLevel = spicyLevel;
-        this.imageUrl = imageUrl;
+    public Order(List<SingleOrder> orderMeals) {
+        this.orderMeals = orderMeals;
     }
 
     public ObjectId getId() {
         return _id;
     }
 
-    public String getName() {
-        return name;
+    public List<SingleOrder> getOrderMeals() {
+        return orderMeals;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
-
-    public String getSize() {
-        return size;
-    }
-
-    public void setSize(String size) {
-        this.size = size;
-    }
-
-    public String getImageUrl() {
-        return imageUrl;
-    }
-
-    public void setImageUrl(String imageUrl) {
-        this.imageUrl = imageUrl;
-    }
-
-
-
-    public double getPrice() {
-        return price;
-    }
-
-    public void setPrice(double price) {
-        this.price = price;
-    }
-
-    public List<CondimentResponse> getToppings() {
-        return toppings;
-    }
-
-    public void setToppings(List<CondimentResponse> toppings) {
-        this.toppings = toppings;
-    }
-
-    public int getSpicyLevel() {
-        return spicyLevel;
-    }
-
-    public void setSpicyLevel(int spicyLevel) {
-        this.spicyLevel = spicyLevel;
-    }
-
-    public Date getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(Date creationDate) {
-        this.creationDate = creationDate;
+    public void setOrderMeals(List<SingleOrder> orderMeals) {
+        this.orderMeals = orderMeals;
     }
 
     public boolean getDeleted() {

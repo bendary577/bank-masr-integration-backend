0b9f885e3ca6193d4e76124e31abb9bef2a7a0bb
Bassel <bassel.faisal.bs@gmail.com>
2021-10-05 09:57:24 +0200

    BWIP web services of zeal

--- src/main/java/com/sun/supplierpoc/services/simphony/CallRestService.java
+++ src/main/java/com/sun/supplierpoc/services/simphony/CallRestService.java
@@ -1,11 +1,8 @@
 package com.sun.supplierpoc.services.simphony;
-import com.sun.supplierpoc.models.simphony.Message;
 import com.sun.supplierpoc.models.simphony.request.ZealLoyaltyRequest;
 import com.sun.supplierpoc.models.simphony.response.ZealLoyaltyResponse;
 import com.sun.supplierpoc.models.simphony.request.ZealRedeemRequest;
 import com.sun.supplierpoc.models.simphony.response.ZealRedeemResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
@@ -22,28 +19,18 @@ public class CallRestService {
 //        this.restTemplate = restTemplateBuilder.build();
     }
 
-
     public ZealLoyaltyResponse zealPayment(ZealLoyaltyRequest zealPayment) {
-//        String url = "https://private-anon-09bdbe61bf-symphonypos.apiary-mock.com/api/visit/scan";
-//        ZealLoyaltyResponse zealLoyaltyResponse =
-//                this.restTemplate.postForObject(url, zealPayment, ZealLoyaltyResponse.class);
-
-        ZealLoyaltyResponse zealLoyaltyResponse = new ZealLoyaltyResponse(
-                "Check paid successfully.", "111", true, true, "Success");
+        String url = "https://macros-pos-production.zeal-members.com/api/visit/scan";
+        ZealLoyaltyResponse zealLoyaltyResponse =
+                this.restTemplate.postForObject(url, zealPayment, ZealLoyaltyResponse.class);
         return zealLoyaltyResponse;
-
     }
 
     public ZealRedeemResponse zealVoucher(ZealRedeemRequest zealRedeemRequest) {
-
-//        String url = "https://private-anon-09bdbe61bf-symphonypos.apiary-mock.com/api/redeem/scan";
-//        ZealRedeemResponse zealRedeemResponse =
-//                this.restTemplate.postForObject(url, zealRedeemRequest, ZealRedeemResponse.class);
-////        logger.info(zealRedeemResponse.toString());
-        ZealRedeemResponse zealRedeemResponse = new ZealRedeemResponse(
-                new Message("Voucher added successfully."), 111, "Success",true);
+        String url = "https://macros-pos-production.zeal-members.com/api/redeem/scan";
+        ZealRedeemResponse zealRedeemResponse =
+                this.restTemplate.postForObject(url, zealRedeemRequest, ZealRedeemResponse.class);
         return zealRedeemResponse;
-
     }
 
 }

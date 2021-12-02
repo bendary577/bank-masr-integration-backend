9bb1de235e63aeb78e0afb8e858792b8e6082eb6
p&p <136>
2021-12-02 14:59:16 +0200

    Fix getting waste groups from OHRA version 2

--- src/main/java/com/sun/supplierpoc/Constants.java
+++ src/main/java/com/sun/supplierpoc/Constants.java
@@ -198,6 +198,7 @@ public class Constants {
 
     public static String MICROS_V2_LINK  = "https://mte4-ohra-idm.oracleindustry.com/oidc-ui/";
     public static String MICROS_REPORT_BASE_LINK = "https://mte4-ohra.oracleindustry.com/portal/";
+    public static String MICROS_INVENTORY_BASE_LINK = "https://mte4-ohra-ohim.oracleindustry.com";
     public static String MICROS_SALES_SUMMARY  = MICROS_REPORT_BASE_LINK + "?root=reports&reports=myReports&myReports=reportGroup&reportGroup=4";
     public static String MICROS_REPORTS = MICROS_REPORT_BASE_LINK + "?root=reports&reports=myReports";
 
@@ -220,7 +221,7 @@ public class Constants {
     public static String MICROS_COS_REPORTS_COST_CENTER = MICROS_REPORT_BASE_LINK + "?root=reports&reports=myReports&myReports=reportGroup&reportGroup=100238";
 
     // WASTAGE LINKS
-    public static String MICROS_WASTE_GROUPS_LINK =  MICROS_REPORT_BASE_LINK + "/InventoryManagement/MasterData/WasteGroups/WasteGroup.aspx";
+    public static String MICROS_WASTE_GROUPS_LINK =  MICROS_INVENTORY_BASE_LINK + "/InventoryManagement/MasterData/WasteGroups/WasteGroup.aspx";
 
     ///////////////////////////////////////// Business Date ////////////////////////////////////////////////////////////
     public static String USER_DEFINED = "UserDefined";

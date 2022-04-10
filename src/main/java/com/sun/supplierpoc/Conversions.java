8bfc644e154cc462297f807aab6b93f660fb7103
lyoussef <lyoussef@entrepreware.com>
2022-04-10 13:54:54 +0200

    WIP in add invoice tax config

--- src/main/java/com/sun/supplierpoc/Conversions.java
+++ src/main/java/com/sun/supplierpoc/Conversions.java
@@ -34,6 +34,15 @@ public class Conversions {
         return new Tax();
     }
 
+    public InforTax checkInforTaxExistence(ArrayList<InforTax> taxes, int taxPercantage) {
+        for (InforTax tax : taxes) {
+            if (tax.taxPercentage == taxPercantage) {
+                return tax;
+            }
+        }
+        return null;
+    }
+
     public Tender checkTenderExistence(ArrayList<Tender> tenders, String tenderName, String location, float amount) {
         for (Tender tender : tenders) {
             if ((tender.getCostCenter().locationName.equals("General") || tender.getCostCenter().locationName.equals(location))
---
+++ src/main/java/com/sun/supplierpoc/models/configurations/InforTax.java
@@ -0,0 +1,6 @@
+package com.sun.supplierpoc.models.configurations;
+
+public class InforTax {
+    public int taxPercentage = 0; // 1 percent
+    public String taxCode = ""; // Predefined code
+}

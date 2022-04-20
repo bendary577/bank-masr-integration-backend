51b0d3405e16c1b8711e8dd63f775e9125057e25
Bassel <bassel.faisal.bs@gmail.com>
2022-04-20 12:55:09 +0200

    edit emaar sales api

--- src/main/java/com/sun/supplierpoc/models/configurations/SalesAPIConfig.java
+++ src/main/java/com/sun/supplierpoc/models/configurations/SalesAPIConfig.java
@@ -10,8 +10,25 @@ public class SalesAPIConfig {
 
     public String apiEndpoint = "";
 
+    public String apiURL = "";
+
     public List<OrderTypeChannels> orderTypeChannels = new ArrayList<>();
 
     public ArrayList<SalesAPIStatistics> statistics = new ArrayList<>();
 
+    public String getApiURL() {
+        return apiURL;
+    }
+
+    public void setApiURL(String apiURL) {
+        this.apiURL = apiURL;
+    }
+
+    public String getApiEndpoint() {
+        return apiEndpoint;
+    }
+
+    public void setApiEndpoint(String apiEndpoint) {
+        this.apiEndpoint = apiEndpoint;
+    }
 }
